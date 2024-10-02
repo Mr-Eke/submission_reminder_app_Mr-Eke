c7ae44d0fc02c2ede05bebefa87e9aaceb9bf91b
El-gibbor <c.eke@alustudent.com>
2024-10-02 23:00:12 +0200

    Implemented script that creates the app environment

---
+++ create_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Creates the app directory and its subdirectories
+mkdir -p submission_reminder_app/{app,modules,assets,config}
+
+cd submission_reminder_app
+
+# Link to the downloaded resources
+ln -s /mnt/c/Users/ekech/Downloads ~/src
+
+# create files with their provided content
+cat ~/src/reminder.sh > app/reminder.sh && chmod +x app/reminder.sh
+cat ~/src/config.env > config/config.env
+cat ~/src/functions.sh > modules/functions.sh && chmod +x modules/functions.sh
+cat ../submissions.txt > assets/submissions.txt
