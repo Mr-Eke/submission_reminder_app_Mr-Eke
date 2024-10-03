b2590cdd8f328defeee41ebc69a48a38590f48f8
El-gibbor <c.eke@alustudent.com>
2024-10-03 12:54:02 +0200

    modified create_environment.sh to populate submission.txt with 5 more records when xcuted

--- create_environment.sh
+++ create_environment.sh
@@ -6,10 +6,15 @@ mkdir -p submission_reminder_app/{app,modules,assets,config}
 cd submission_reminder_app
 
 # Link to the downloaded resources
-ln -s /mnt/c/Users/ekech/Downloads ~/src
+ln -sf /mnt/c/Users/ekech/Downloads ~/src
 
 # create files with their provided content
 cat ~/src/reminder.sh > app/reminder.sh && chmod +x app/reminder.sh
 cat ~/src/config.env > config/config.env
 cat ~/src/functions.sh > modules/functions.sh && chmod +x modules/functions.sh
 cat ../submissions.txt > assets/submissions.txt
+
+# add 5 more additional student records to submissions.txt file
+echo -e "Simeon, Shell Navigation, not submitted\nKevin, Shell Navigation, submitted\
+Eke, Shell Navigation, submitted\nSwanikar, Shell Navigation, not submitted\
+\nFred, Shell Navigation, not submitted" >> assets/submissions.txt
